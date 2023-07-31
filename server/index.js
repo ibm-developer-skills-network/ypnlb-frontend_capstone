163833dee1484118b3d3b95ea7a4c9adbf1e3d2b
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-07-31 11:14:15 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 const connectToMongo = require('./db');
 const app = express();
-
+const path = require('path');
 const PORT = process.env.PORT || 8181;
 
 
@@ -17,9 +16,11 @@ connectToMongo();
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
-//React app
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
